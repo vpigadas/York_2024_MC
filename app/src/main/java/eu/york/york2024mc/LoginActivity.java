46f34fb3a22e88467ffdd5f56a7fcca0ba11f7f0
vpigadas <v.pigadas@printecgroup.com>
2024-04-02 00:19:25 +0300

    navigation

--- app/src/main/java/eu/york/york2024mc/LoginActivity.java
+++ app/src/main/java/eu/york/york2024mc/LoginActivity.java
@@ -1,14 +1,63 @@
 package eu.york.york2024mc;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
+import eu.york.york2024mc.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_login);
+        setContentView(binding.getRoot());
+
+        TextView textView = findViewById(R.id.title);
+        TextView title = binding.title;
+
+        EditText username = binding.editUsername;
+
+        Button button = binding.btnLogin;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Hello World!!", Snackbar.LENGTH_LONG).show();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("name", "Vassilis");
+                intent.putExtra("age", 100);
+//                startActivity(intent);
+                startActivityForResult(intent,2000);
+            }
+        });
+
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(LoginActivity.this, "From Toast", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String name = data.getStringExtra("name");
+        int age = data.getIntExtra("age",0);
     }
 }
